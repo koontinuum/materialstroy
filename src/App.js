b0838d4bf97d02946c6885866bb31db71d899a79
Your Name <you@example.com>
2023-05-01 13:50:28 +0600

    feat/header and slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.scss'
+import Header from './components/header/Header';
+import NavBar from './components/navBar/NavBar';
+import SliderComponent from './components/slider/SliderComponent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Header />
+      <SliderComponent />
     </div>
   );
 }
