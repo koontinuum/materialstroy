58caf75f7304e6f14187c015adf66307d6ca3907
Your Name <you@example.com>
2023-05-03 00:27:27 +0600

    feat/ContactBtn and fixes components

---
+++ src/constants/differentObjects.js
@@ -0,0 +1,16 @@
+import bagImg from '../assets/navBarIcons/bagImg.svg'
+
+export const navIcons = [
+    {
+        id: 1,
+        img: bagImg
+    },
+    {
+        id: 2,
+        img: bagImg
+    },
+    {
+        id: 3,
+        img: bagImg
+    },
+]
