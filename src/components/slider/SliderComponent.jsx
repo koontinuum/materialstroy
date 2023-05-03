f8be23e9c2bbc7052735fa342cb65615f9263c71
Your Name <you@example.com>
2023-05-03 14:59:39 +0600

    feat/contactBtn

--- src/components/slider/SliderComponent.jsx
+++ src/components/slider/SliderComponent.jsx
@@ -57,7 +57,7 @@ const SliderComponent = () => {
       </button>
       <Slider ref={arrowRef} {...settings}>
         {slideProducts.map((item) => (
-          <div className={styles.card}>
+          <div key={item.id} className={styles.card}>
             <div className={styles.product}>
               <img src={item.img} alt={item.title} />
               <b>{item.title}</b>
